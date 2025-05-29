be805e41a50628320c75762bf3c7e92ac4c96ac9
sultan <sultandan216@gmail.com>
2025-05-29 22:34:15 +0700

    pause

--- sund-res/js/profdata.js
+++ sund-res/js/profdata.js
@@ -1,15 +1,4 @@
-const menuIcon = document.getElementById("menu-icon");
-const dropdown = document.getElementById("dropdown");
 
-menuIcon.addEventListener("click", () => {
-  dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
-});
-
-document.addEventListener("click", (e) => {
-  if (!menuIcon.contains(e.target) && !dropdown.contains(e.target)) {
-    dropdown.style.display = "none";
-  }
-});
    function loadProfile() {
       document.getElementById('nameDisplay').innerText = localStorage.getItem('profileName') || 'Belum diisi';
       document.getElementById('phoneDisplay').innerText = localStorage.getItem('profilePhone') || 'Belum diisi';
